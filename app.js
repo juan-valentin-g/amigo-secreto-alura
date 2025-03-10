04767a2b5c8d002b4716efa619a772e547b1b55f
Juan Valentin <juanvalentinanto12@gmail.com>
2025-03-10 01:47:49 -0600

    Acutalizacion de el documento js para la logica de mi programa

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if(nombreAmigo == ''){
+        return alert('Ingrese un nombre valido')
+    }
+    console.log(nombreAmigo);
+    amigos.push(nombreAmigo);
+    console.log(amigos);
+    document.getElementById('amigo').value = '';
+    mostrarLista();
+}
+
+function mostrarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for(let i=0; i < amigos.length; i++){
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+        console.log(amigos[i]);
+    }
+}
+
+function sortearAmigo() {
+    
+}
+
